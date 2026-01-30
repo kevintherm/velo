7208106e18fb94d9a1649004094272add414097f
Kevin Darmawan <kevindarmawan022@gmail.com>
2026-01-30 15:39:34 +0700

    removed dangerous schedule command

--- routes/console.php
+++ routes/console.php
@@ -8,10 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Schedule::command('queue:work --stop-when-empty --timeout=120 --memory=128')
-//    ->everyMinute()
-//    ->withoutOverlapping();
-
 Schedule::call(function () {
     \App\Models\RealtimeConnection::pruneStale();
 })->everyMinute();
